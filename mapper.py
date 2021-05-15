687ff7818620e4bb4821d840bc503bf2a3b34ffc
lhennes2 <lucashennessey5@gmail.com>
2021-05-15 22:06:48 +0000

    lowecase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import sys
 
+stopwords = set(['the', 'and', 'i', 'to', 'of', 'a', 'you', 'if', 'which', 'when', 'my', 'that', 'is', 'not','in', 'me', 'it', 'for', 'with', 'be', 'your', 'this', 'his', ',', '.', '[', ']', '(', ')', '&', '/', '-', '?', '!', '$','%', ';', ':', '`', '~', '*', '^']) 
+
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
-
-    # split the line into words; splits on any whitespace
+    line = line.strip().lower()
+   
+ # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwords:
+        	print '%s\t%s' % (word, "1")
